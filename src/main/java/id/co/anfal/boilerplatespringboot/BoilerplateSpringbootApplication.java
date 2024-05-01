915b23215c6261bffa23c8317bb9fe992c3321b2
Singgih Pratama <singgih.vendor.pasti@pegadaian.co.id>
2024-05-01 11:18:27 +0700

    initialize project boilerplate spring boot

---
+++ src/main/java/id/co/anfal/boilerplatespringboot/BoilerplateSpringbootApplication.java
@@ -0,0 +1,13 @@
+package id.co.anfal.boilerplatespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerplateSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoilerplateSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/co/anfal/boilerplatespringboot/BoilerplateSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.anfal.boilerplatespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerplateSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
